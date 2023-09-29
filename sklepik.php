9fe43f7b20597f65e3e27cb5aaa42cda683cf7c0
adam2973 <91157372+adam2973@users.noreply.github.com>
2023-09-29 14:40:19 +0200

    Create sklepik.php
    ddddd

---
+++ sklepik.php
@@ -0,0 +1,19 @@
+<?php
+$p=$_GET["imie"];
+$p1=$_GET["imie1"];
+$p2=$_GET["imie2"];
+$p3=$_GET["imie3"];
+$p4=$_GET["imie4"];
+$p5=$_GET["imie5"];
+$p6=$_GET["imie6"];
+$p7=$_GET["imie7"];
+$p8=$_GET["imie8"];
+$p9=$_GET["imie9"];
+$p10=$_GET["imie10"];
+$p11=$_GET["imie11"];
+//isset($p=$_GET["imie2"]);
+echo "Imię i nazwisko: " , $p , " " , $p1 , "<br>Co lubisz bardziej:", $p2 ,"<br>Jakiej plci jestes: " , $p3 , "<br>" , "Jakie napoje lubisz pić: " , $p4 ," ", $p5 ," ", $p6 ," ", $p7 , "<br> Jakiego rodzaju napój lubisz pić: " , $p8 , "<br> Jaką pojemność butelki najczęściej wybierasz: ", $p9 , "<br> Podaj swój komentarz: " , $p10 ;
+ 
+echo $p11;
+?>
+ 
